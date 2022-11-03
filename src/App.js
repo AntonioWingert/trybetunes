ae956920cfaeb95e23fab057fcd8be93a75b6294
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-11-03 14:54:42 -0300

    req 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/album/:id">
+          <Album />
+        </Route>
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/profile">
+          <Profile />
+        </Route>
+        <Route exact path="/profile/edit">
+          <ProfileEdit />
+        </Route>
+        <Route path="">
+          <NotFound />
+        </Route>
+      </Switch>
+    );
   }
 }
 
