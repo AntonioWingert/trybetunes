32844d5d9103f9a8e27c948b996a3910f6c181f9
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-11-07 14:06:50 -0300

    req 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
 import Header from './components/Header/Header';
+import Loading from './components/Loading/Loading';
 
 class Profile extends Component {
+  state = {
+    isLoading: false,
+    userInfo: {},
+  };
+
+  componentDidMount() {
+    this.receiveUser();
+  }
+
+  receiveUser = async () => {
+    this.setState({ isLoading: true });
+    const user = await getUser();
+    this.setState({
+      isLoading: false,
+      userInfo: user,
+    });
+  };
+
   render() {
+    const { isLoading, userInfo } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        {isLoading && <Loading />}
+        <div>
+          <img
+            src={ userInfo.image }
+            data-testid="profile-image"
+            alt={ userInfo.name }
+          />
+          <Link to="/profile/edit">Editar perfil</Link>
+          <p>Nome</p>
+          <span>{userInfo.name}</span>
+          <p>Email</p>
+          <span>{userInfo.email}</span>
+          <p>Descrição</p>
+          <span>{userInfo.description}</span>
+        </div>
       </div>
     );
   }
